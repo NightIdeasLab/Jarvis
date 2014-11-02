56e91f0f67edaa7bb1f92a37b2a4b20949b8ec9a
Gabe <ulicigabriel@gmail.com>
2014-11-02 20:47:58 +0100

    sparkle fixes

--- Jarvis/AppDelegate.m
+++ Jarvis/AppDelegate.m
@@ -13,7 +13,7 @@
 #define DONATE_URL  @"http://goo.gl/YzTfe"
 #define DONATE_NAG_TIME (60 * 60 * 24 * 7)
 
-#define SLOW_INTERNET 0 // 1 for YES 0 otherwise. Sometimes my internet connection suckes, in this way i can still code :)
+#define SLOW_INTERNET 1 // 1 for YES 0 otherwise. Sometimes my internet connection suckes, in this way i can still code :)
 
 NSSpeechSynthesizer *synth;
 
@@ -87,7 +87,8 @@ NSSpeechSynthesizer *synth;
         [NSApp setDelegate: self];
         
         // initializing the update system
-        [[SUUpdater sharedUpdater] setDelegate: self];
+        //[[SUUpdater sharedUpdater] setDelegate: self];
+        [SUUpdater sharedUpdater];
     }
     return self;
 }
